3b2e7c26d22ec1ccefbf7e6cbb8657c5bfa59464
Rasyidannas <rasshit.dsgn@gmail.com>
2024-05-01 18:15:14 +0700

    lesson7.mp4

--- first_script.sh
+++ first_script.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 mkdir -p dir1
+# This is a Bash comment
+:'
+This is a very
+cool multiline
+comment in Bash
+date; ls
+'
 echo "Hello Bash World!!" > dir1/file.txt
 tree .
 cat dir1/file.txt
